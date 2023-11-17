a0b3f82c2a28ab908e18143f3135ce0918d0ab6b
lzelaya-aa <litzyzelayaa@gmail.com>
2023-11-16 21:27:00 -0600

    updated foster.html n added a fostercats.php test

---
+++ fostercats.php
@@ -0,0 +1,67 @@
+<?php
+$fostercats = [
+    [
+        'name' => 'Grumps',
+        'gender' => 'Female',
+        'good_with_cats' => 'Yes',
+        'good_with_dogs' => 'Yes',
+        'good_with_kids' => 'No',
+        'img_src' => './images/angry-cat.png',
+    ],
+
+    [
+        'name' => 'Mr. cool',
+        'gender' => 'Male',
+        'good_with_cats' => 'Yes',
+        'good_with_dogs' => 'Yes',
+        'good_with_kids' => 'Yes',
+        'img_src' => './images/cool-cat.jpg',
+    ],
+
+    [
+        'name' => 'Bob',
+        'gender' => 'Male',
+        'good_with_cats' => 'Yes',
+        'good_with_dogs' => 'No',
+        'good_with_kids' => 'Yes',
+        'img_src' => './images/minion-cat.png',
+    ],
+
+    [
+        'name' => 'Sharpay',
+        'gender' => 'Female',
+        'good_with_cats' => 'Yes',
+        'good_with_dogs' => 'No',
+        'good_with_kids' => 'Yes',
+        'img_src' => './images/shades-cat.jpg',
+    ],
+
+    [
+        'name' => 'Mouse',
+        'gender' => 'Male',
+        'good_with_cats' => 'Yes',
+        'good_with_dogs' => 'No',
+        'good_with_kids' => 'Yes',
+        'img_src' => './images/wirecat.jpg',
+    ],
+
+    [
+        'name' => 'Mary',
+        'gender' => 'Female',
+        'good_with_cats' => 'Yes',
+        'good_with_dogs' => 'Yo',
+        'good_with_kids' => 'No',
+        'img_src' => './images/smoking-cat.jpg',
+    ],
+
+    [
+        'name' => 'Robin',
+        'gender' => 'Male',
+        'good_with_cats' => 'Yes',
+        'good_with_dogs' => 'Yes',
+        'good_with_kids' => 'Yes',
+        'img_src' => './images/sad-cat.jpg',
+    ],
+    
+];
+?>
