2f43fb6aa242127d6fa9bca4e7b7aed85775f5f5
lzelaya-aa <litzyzelayaa@gmail.com>
2023-11-18 14:20:58 -0600

    updated the db_pass in config.php

--- php-scripts/config.php
+++ php-scripts/config.php
@@ -2,7 +2,7 @@
 // Database configuration
 define('DB_HOST', 'kch-db.mysql.database.azure.com');
 define('DB_USER', 'adminkch');
-define('DB_PASS', 'adminkch777!');
+define('DB_PASS', 'kchpwd777!');
 define('DB_NAME', 'kittycomehomedb');
 
 $db_conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
