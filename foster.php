845597dd6203be094f1f58ee066743cb268c0113
lzelaya-aa <litzyzelayaa@gmail.com>
2023-11-18 13:24:12 -0600

    updated foster,php

--- foster.php
+++ foster.php
@@ -1,18 +1,3 @@
-<?php
-// foster.php
-
-include 'navbar.php';
-include 'php-scripts/config.php';
-
-// Query to fetch cat information from the database
-$query = "SELECT cat_name, cat_img_src FROM fostercat";
-$result = $db_conn->query($query);
-// Check if the query was successful
-if (!$result) {
-    die("Error in query: " . $db_conn->error);
-}
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -34,21 +19,48 @@ if (!$result) {
     <div class="gallery-container">
         <!-- Loops through cat names -->
         <?php
-        if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            ?>
-            <div class="card">
-                <div class="kitty-card">
-                    <img src="<?php echo $row['cat_img_src']; ?>" alt="<?php echo $row['cat_name']; ?>">
-                    <p><?php echo $row['cat_name']; ?></p>
-                </div>
-            </div>
-            <?php
+        // Database configuration
+        $db_host = 'kch-db.mysql.database.azure.com';
+        $db_user = 'adminkch';
+        $db_pass = 'adminkch777!';
+        $db_name = 'kittycomehomedb';
+
+        // Create a connection
+        $db_conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+        // Check the database connection
+        if ($db_conn->connect_error) {
+            die("Connection failed: " . $db_conn->connect_error);
         }
-    } else {
-        // Display a message if there are no cat cards
-        echo "<p>No cat cards available.</p>";
-    }
+
+        // Query to fetch cat information from the database
+        $query = "SELECT cat_name, cat_img_src FROM fostercat";
+        $result = $db_conn->query($query);
+
+        // Check if the query was successful
+        if (!$result) {
+            die("Error in query: " . $db_conn->error);
+        }
+
+        // Loop through cat names
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                ?>
+                <div class="card">
+                    <div class="kitty-card">
+                        <img src="<?php echo $row['cat_img_src']; ?>" alt="<?php echo $row['cat_name']; ?>">
+                        <p><?php echo $row['cat_name']; ?></p>
+                    </div>
+                </div>
+                <?php
+            }
+        } else {
+            // Display a message if there are no cat cards
+            echo "<p>No cat cards available.</p>";
+        }
+
+        // Close the database connection
+        $db_conn->close();
         ?>
     </div>
 </body>
