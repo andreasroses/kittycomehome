80c11f7303b6f3e94f799690ab7bef9e689e6955
lzelaya-aa <litzyzelayaa@gmail.com>
2023-11-18 14:52:01 -0600

    updated the config.php text error

--- php-scripts/config.php
+++ php-scripts/config.php
@@ -3,13 +3,14 @@
 define('DB_HOST', 'kch-db.mysql.database.azure.com');
 define('DB_USER', 'adminkch');
 define('DB_PASS', 'kchpwd777!');
+define('DB_PORT', 3306);
 define('DB_NAME', 'kittycomehomedb');
 
-$db_conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
 // Check the database connection
 if ($db_conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $mysqli->connect_error);
 } 
 
 ?>
