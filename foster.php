fa237b2f5c98bb2794e8b3735d8350f0258d797b
lzelaya-aa <litzyzelayaa@gmail.com>
2023-11-18 16:58:53 -0600

    updated foster.php

--- foster.php
+++ foster.php
@@ -16,16 +16,17 @@
     </div>
     <div><button><a href="./images/fostering-form.pdf" class ="home-sub-header">Download fostering Form</a></button></div>
     -->
-    <?php echo'Before testing database connection';?>
+    <?php echo'Before testing database connection'; //debug ?> 
     <div class="gallery-container">
         
         <!-- Loops through cat names -->
         <?php
-        echo'About to test database connection';
+        echo'About to test database connection'; // debug
         // Include the database configuration
         include 'php-scripts/config.php';
         echo "Connected to the database successfully."; // debug
-        // Query to fetch cat information from the database
+
+        //Query to fetch cat information from the database
         $query = "SELECT cat_name, cat_img_src FROM fostercat";
         $result = $mysqli->query($query);
 
@@ -33,33 +34,27 @@
         if (!$result) {
             die("Error in query: " . $mysqli->error);
         }else {
-            echo "Test query executed successfully.";
-        }
-        // Close the database connection
-        $mysqli->close();
-        ?>
-        <!--
-        // Query to fetch cat information from the database
-        $query = "SELECT cat_name, cat_img_src FROM fostercat";
-        $result = $mysqli->query($query);
-
-        // Check if the query was successful
-        if (!$result) {
-            die("Error in query: " . $mysqli->error);
-        }else {
-            echo "Test query executed successfully.";
+           echo "Test query executed successfully.";
         }
         // Loop through cat names
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
-                ?>
-                <div class="card">
-                    <div class="kitty-card">
+               ?>
+            <div class="card">
+                   <div class="kitty-card">
                         <img src="/<?php echo $row['cat_img_src']; ?>" alt="<?php echo $row['cat_name']; ?>">
                         <p><?php echo $row['cat_name']; ?></p>
                     </div>
                 </div>
-            -->
+                <?php
+            }
+        } else {
+            // Display a message if there are no cat cards
+            echo "<p>No cat cards available.</p>";
+        }
+        // Close the database connection
+        $mysqli->close();
+        ?>
     </div>
 </body>
 </html>
