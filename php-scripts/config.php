ca1233b753fbc2553d4549646b0c5867f53575a3
Andrea Mendez <andreacelestem11@gmail.com>
2023-11-18 17:47:31 -0600

    removing port from connection

--- php-scripts/config.php
+++ php-scripts/config.php
@@ -3,10 +3,9 @@
 define('DB_HOST', 'kch-db.mysql.database.azure.com');
 define('DB_USER', 'adminkch');
 define('DB_PASS', 'kchpwd777!');
-define('DB_PORT', 3306);
 define('DB_NAME', 'kittycomehomedb');
 
-$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 // Check the database connection
 if ($mysqli->connect_error) {
