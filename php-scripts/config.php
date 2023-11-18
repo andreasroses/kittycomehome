8f9656c2dae6ac4a7ae0ce4fa1fa8e26bef28f8e
lzelaya-aa <litzyzelayaa@gmail.com>
2023-11-18 16:33:23 -0600

    set charset for config.php

--- php-scripts/config.php
+++ php-scripts/config.php
@@ -10,8 +10,9 @@ $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
 // Check the database connection
 if ($mysqli->connect_error) {
-    echo "Connection failed: " . $mysqli->connect_error; //debugging
-    //die("Connection failed: " . $mysqli->connect_error);
-} 
+    die("Connection failed: " . $mysqli->connect_error);
+}
+// Set charset to utf8mb4 for proper Unicode support
+$mysqli->set_charset("utf8mb4");
 
 ?>
