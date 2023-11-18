f4078a7fea2925ea5bc14854554ee64251f04370
lzelaya-aa <litzyzelayaa@gmail.com>
2023-11-18 17:04:40 -0600

    updated foster.php

--- foster.php
+++ foster.php
@@ -18,10 +18,8 @@
     -->
     <?php echo'Before testing database connection'; //debug ?> 
     <div class="gallery-container">
-        
         <!-- Loops through cat names -->
         <?php
-        echo'About to test database connection'; // debug
         // Include the database configuration
         include 'php-scripts/config.php';
         echo "Connected to the database successfully."; // debug
@@ -35,10 +33,25 @@
         }else {
             echo "Test query executed successfully.";
         }
+        // Loop through cat names
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                ?>
+                <div class="card">
+                    <div class="kitty-card">
+                        <img src="<?php echo $row['cat_img_src']; ?>" alt="<?php echo $row['cat_name']; ?>">
+                        <p><?php echo $row['cat_name']; ?></p>
+                    </div>
+                </div>
+                <?php
+            }
+        } else {
+            // Display a message if there are no cat cards
+            echo "<p>No cat cards available.</p>";
+        }
         // Close the database connection
         $mysqli->close();
         ?>
-
     </div>
 </body>
 </html>
