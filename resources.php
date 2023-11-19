f0433463525266ea605f2a201292abcb69e6badd
lzelaya-aa <litzyzelayaa@gmail.com>
2023-11-19 16:59:02 -0600

    updated resources.php

--- resources.php
+++ resources.php
@@ -4,8 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>resources</title>
+    <link rel="stylesheet" href="stylesheets/style.css">
 </head>
 <body>
-    
+    <body style="background-image: url(''); background-repeat: repeat; background-size: 10%;">
+        <div class="flex">
+            <?php include 'php-scripts/navbar.php'; ?>
+        </div>
+        <!--header image-->
+    <div class="header_img">
+        <img src="./images/cat-glare-banner.jpeg" alt="A picture of cute cat paws behind pink background.">
+    </div>
+    <!--resources-->
+    <div class="resource-container">
+        <div class="resource-display">
+            <img src="https://www.biodev.org/wp-content/uploads/2022/09/pasted-image-0-16.jpg" alt="ASPCA logo"> <br>
+            <P>For over a century, 
+            ASPCA, a prominent national animal welfare organization, has been dedicated to the mission of preventing cruelty towards animals.</P>
+            <a href="https://www.aspca.org/pet-care/cat-care">ASPCA</a>
+        </div>
+        <div class="resource-display">
+            <img src="https://mma.prnewswire.com/media/2070060/petkeen_logo_dark_Logo.jpg?p=facebook" alt="PetKeen logo"> <br>
+            <P>Run by a board comprising veterinarians, veterinary technicians, and animal trainers,
+                Petkeen provides well-researched and scientifically-backed advice, covering all aspects of feline care.</P>
+            <a href="https://petkeen.com/category/cats/">PetKeen</a>
+        </div>
+        <div class="resource-display">
+            <img src="https://shelter-assets.shelterbeds.org/file/public-shelterbeds/shelter-logo/358676.jpg?t=9f6c548a43a06e10a105d23ac503df45" alt="Best Friends logo"> <br>
+            <P>A nonprofit organization that operates a vast sanctuary for homeless animals, actively contributing to adoption initiatives,
+                spay/neuter programs, and educational outreach for the welfare of animals.</P>
+            <a href="https://resources.bestfriends.org/pet-health/cats-health">Best Friends</a>
+        </div>
+        <div class="resource-display">
+            <img src="https://vcahospitals.com/-/media/2/vca/images/global/vca-logo_alert-banner3.jpg" alt="VCA Hostpitals logo"> <br>
+            <P>Provided by a solid network of reputable veterinary hospitals this site is an excellent resource for locating facilities in your area. Moreover,
+                it offers a wealth of pet health articles and valuable tips covering nutrition, therapies, behavior, breeding, and various other aspects</P>
+            <a href="https://vcahospitals.com/">VCA Hostpitals</a>
+        </div>
+    </div>
+
 </body>
 </html>
