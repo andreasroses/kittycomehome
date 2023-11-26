dd69b1bd1cd4f5803ab87fc8e1db65e920aec680
smurray769 <91291246+smurray769@users.noreply.github.com>
2023-11-26 15:19:23 -0600

    removed favorites link from nav bar since it's being handled differently now

--- php-scripts/navbar.php
+++ php-scripts/navbar.php
@@ -4,12 +4,10 @@
 if (isset($_SESSION['user_id'])) {
     // User is logged in
     $navbarLink = '<a href="useraccount.php" class="nav-link-end">My Account</a>';
-    $fav = '<li><a href="favorites.php" class="nav-link">Favorites</a></li>';
     $list = '<li><a href="listCat.php" class="nav-link">List a cat</a></li>';
 } else {
     // User is not logged in
     $navbarLink = '<a href="login-pg.php" class="nav-link-end">Sign-up/Login</a>';
-    $fav = '';
     $list = '';
 }
 ini_set('display_errors', 1);
@@ -29,7 +27,6 @@ error_reporting(E_ALL);
         </li>
         <?php echo $navbarLink; ?>
         <div class="vl"></div>
-        <?php echo $fav; ?>
         <?php echo $list; ?>
         <li><a href="resources.php" class="nav-link">Resources</a></li>
         <li><a href="foster.php" class="nav-link">Foster</a></li>
