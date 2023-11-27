6d63e78ad3e67cb2f9a96d81ba5872ebfa41ebb0
wguzc <wendy.guzman-castro@my.utsa.edu>
2023-11-26 18:48:38 -0600

    Update forgot_password.php
    attempt 3 at forgot password

---
+++ php-scripts/forgot_password.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    require_once("config.php");
+
+    $email = $_POST["email"];
+    $sql = "SELECT * FROM accounts WHERE account_email = '$email'";
+    $result = $db_conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $securityQuestion = $row['security_question_id'];
+        $storedAnswer = $row['security_answer'];
+
+        // Check if the security question and answer are set
+        if ($securityQuestion !== null && $storedAnswer !== null) {
+            // Display the security question
+            echo "<p>Security Question: " . getSecurityQuestion($securityQuestion) . "</p>";
+
+            // Allow the user to input the answer
+            echo '<form action="./reset_password.php" method="POST">';
+            echo '<input type="hidden" name="email" value="' . $email . '">';
+            echo '<input type="hidden" name="securityQuestion" value="' . $securityQuestion . '">';
+            echo '<label for="securityAnswer">Answer:</label>';
+            echo '<input type="text" id="securityAnswer" name="securityAnswer" required>';
+            echo '<input type="submit" value="Submit">';
+            echo '</form>';
+        } else {
+            echo "<p>Security question not set for this account. Please contact support.</p>";
+        }
+    } else {
+        echo "<p>User not found.</p>";
+    }
+}
+
+function getSecurityQuestion($questionId, $db_conn) {
+    $questionId = intval($questionId);  // Ensure that $questionId is an integer to prevent SQL injection
+
+    $sql = "SELECT security_question_text FROM account_security WHERE security_question_id = $questionId";
+    $result = $db_conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        return $row['security_question_text'];
+    } else {
+        return "Security question not found";
+    }
+}
+?>
