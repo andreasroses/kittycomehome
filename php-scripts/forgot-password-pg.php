4955a5eaacaf2915632fdf1f34d4e684536f1a49
wguzc <wendy.guzman-castro@my.utsa.edu>
2023-11-26 18:36:09 -0600

    attempt 1 for reset password and reset password page

---
+++ php-scripts/forgot-password-pg.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forgot Password</title>
+    <link rel="stylesheet" href="stylesheets/style.css">
+    <link rel="stylesheet" href="stylesheets/formStyle.css">
+    <link href="https://fonts.googleapis.com/css2?family=Playpen+Sans:wght@800&display=swap" rel="stylesheet">
+</head>
+
+<body style="min-height: 100vh;">
+    <div class="flex">
+        <?php include 'php-scripts/navbar.php'; ?>
+    </div>
+    <!--header image-->
+    <div class="flex maincontainer">
+        <div class="formBox">
+            <form action="./php-scripts/forgot_password.php" method="POST">
+                <table class="signupForm">
+                    <tr>
+                        <td colspan="2">
+                            <h1>Forgot Password</h1>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <label for="email">Email:</label>
+                            <input class="form-control" style="width: 95%" type="text" id="email" name="email" required>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <div class="submit-button-container">
+                                <input class="list-form-btn" type="submit" value="Submit">
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div>
+    <footer class="footerabs"></footer>
+</body>
+
+</html>
