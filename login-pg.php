4b9d49a595c9cc8ad6df69e407df77f2a54301d8
wguzc <wendy.guzman-castro@my.utsa.edu>
2023-11-26 18:39:34 -0600

    updating login-pg
    updating login-pg to hyperlink the forgot password pg

--- login-pg.php
+++ login-pg.php
@@ -46,7 +46,7 @@
                         <td>
                             <label for="pwd">Password:</label>
                             <input class="form-control" type="password" id="pwd" name="pwd">
-                            <h4>Forgot password?</h4><br>
+                            <h4><a href="forgot-password-pg.php">Forgot password?</a></h4><br>
                             <div class="submit-button-container">
                                 <input class="list-form-btn" type="submit" value="Login">
                             </div>
