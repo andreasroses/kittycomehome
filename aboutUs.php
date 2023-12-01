d314ffcbe3b82575deaa6a7f8db12cb139607e6b
Andrea Mendez <andreacelestem11@gmail.com>
2023-11-30 18:36:55 -0600

    minor change to some pg titles

--- aboutUs.php
+++ aboutUs.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>aboutUs</title>
+    <title>About Us</title>
     <link rel="stylesheet" href="stylesheets/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
